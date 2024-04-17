2018b368c76e9c08968406aa8f6121b5eff493cd
Charles Eckman <charles@cceckman.com>
2024-04-17 16:55:02 -0400

    Add (and default to) lightness gradient

--- app.js
+++ app.js
@@ -1,6 +1,76 @@
 
 const canvas = document.getElementById("draw-here");
 
+class HueGradient{
+    constructor() {
+        this.sat = Math.round(Math.random() * 70 + 30);
+        this.val = Math.round(Math.random() * 70 + 30);
+        this.start_color = Math.round(Math.random() * 360);
+        this.speed = Math.round(Math.random() * 360);
+    }
+
+    gradient_for_column(ctx, height, column) {
+        const start_color = (this.start_color + column * this.speed) % 360;
+        const mid_color = (start_color + 90) % 360;
+        const end_color = (start_color + 180) % 360;
+        const start_str = `hsl(${start_color}deg, ${this.sat}%, ${this.val}%)`;
+        const mid_str = `hsl(${mid_color}deg, ${this.sat}%, ${this.val}%)`;
+        const end_str = `hsl(${end_color}deg, ${this.sat}%, ${this.val}%)`;
+
+        // console.log("colors: ", start_str, end_str)
+        // Gradient only in the vertical dimension.
+        const grad = ctx.createLinearGradient(0, 0, 0, height);
+        grad.addColorStop(0, start_str);
+        grad.addColorStop(0.5, mid_str);
+        grad.addColorStop(1, end_str);
+        return grad;
+    }
+}
+
+function to_pct(v) {
+    return Math.round(v * 100);
+}
+
+class LightnessGradient{
+    constructor() {
+        this.sat = to_pct(Math.random() * 90 + 10);
+        this.color = Math.round(Math.random() * 360);
+        // Use sin(sat) so we get a nice back-and-forth, like real yarn
+        this.start_val = Math.round(Math.random() * 360);
+
+        const a = Math.random();
+        const b = Math.random();
+        this.min_val = Math.min(a, b);
+        this.max_val = Math.max(a, b);
+        this.speed = Math.round(Math.random() * 360);
+    }
+
+    angle_to_pct(angle) {
+        let fraction = Math.sin(angle);
+        let v = this.min_val + (this.max_val - this.min_val) * fraction;
+        return to_pct(v);
+    }
+
+    gradient_for_column(ctx, height, column) {
+        // Sped is- kinda unitless?
+        const start = (this.start_val + column * this.speed);
+        const mid = start + this.speed;
+        const end = mid + this.speed;
+
+        const start_str = `hsl(${this.color}deg, ${this.sat}%,  ${this.angle_to_pct(start)}%)`;
+        const mid_str = `hsl(${this.color}deg, ${this.sat}%,    ${this.angle_to_pct(mid)}%)`;
+        const end_str = `hsl(${this.color}deg, ${this.sat}%,    ${this.angle_to_pct(end)}%)`;
+
+        console.log("colors:", start_str, mid_str, end_str)
+
+        const grad = ctx.createLinearGradient(0, 0, 0, height);
+        grad.addColorStop(0, start_str);
+        grad.addColorStop(0.5, mid_str);
+        grad.addColorStop(1, end_str);
+        return grad;
+    }
+}
+
 // Use https://stackoverflow.com/questions/4288253/html5-canvas-100-width-height-of-viewport ?
 function rerender() {
     canvas.height = canvas.clientHeight;
@@ -16,40 +86,24 @@ function rerender() {
     const params = new URLSearchParams(window.location.search);
     const stitchwidth = Number(params.get("stitchwidth") ?? 19);
     const stitchheight = Number(params.get("stitchheight") ?? 13);
-    const threads = Number(params.get("threads") ?? 7);
+    const threads = Number(params.get("threads") ?? 3);
+    const mode = params.get("mode") ?? "value";
     console.log("params:", stitchwidth, stitchheight, threads);
 
-    // For each column, create numthreads gradients, starting at different points.
     for (let col = 0; col < threads; col++) {
-        let sat = Math.round(Math.random() * 70 + 30);
-        let val = Math.round(Math.random() * 70 + 30);
-        let start_color = Math.round(Math.random() * 360);
-        let speed = Math.round(Math.random() * 360);
-        gradient_params.push({
-            start_color: start_color, sat: sat, val: val, speed: speed,
-        });
+        let constructor;
+        if(mode === "hue") {
+            constructor = HueGradient;
+        } else if (mode === "value") {
+            constructor = LightnessGradient;
+        }
+        gradient_params.push(new constructor());
     }
 
     let thread = 0;
-    for(let row = 0; row < (w / stitchwidth); row++) {
-        const x0 = row * stitchwidth;
-        let gradients = [];
-        for (let g of gradient_params) {
-            const start_color = (g.start_color + row * g.speed) % 360;
-            const mid_color = (start_color + 90) % 360;
-            const end_color = (start_color + 180) % 360;
-            const start_str = `hsl(${start_color}deg, ${g.sat}%, ${g.val}%)`;
-            const mid_str = `hsl(${mid_color}deg, ${g.sat}%, ${g.val}%)`;
-            const end_str = `hsl(${end_color}deg, ${g.sat}%, ${g.val}%)`;
-
-            // console.log("colors: ", start_str, end_str)
-            // Gradient only in the vertical dimension.
-            const grad = ctx.createLinearGradient(x0, 0, x0, h);
-            grad.addColorStop(0, start_str);
-            grad.addColorStop(0.5, mid_str);
-            grad.addColorStop(1, end_str);
-            gradients.push(grad);
-        }
+    for(let column = 0; column < (w / stitchwidth); column++) {
+        const x0 = column * stitchwidth;
+        let gradients = gradient_params.map((g) => g.gradient_for_column(ctx, h, column));
         for (let y0 = 0; y0 < h; y0 += stitchheight) {
             ctx.fillStyle = gradients[thread];
             thread = (thread + 1) % threads;
